5219946af7844c07818ac496f6e867417766634d
Sans2k <140954881+Sans2k@users.noreply.github.com>
2023-11-01 15:01:54 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.border}>
+        <Text>Nama : Sansan</Text>
+        <Text>Nis : 22310281</Text>
+        <Text>Kelas : IX PPLG 3</Text>
+        <Text>Alamat : Pelang</Text>
+        <Text>Hobby : Ngaji</Text>
+        <View style={styles.button}>
+          <Button
+            title='Press me!'
+            onPress={() => alert('Dor!!!')}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aqua',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  border: {
+    borderWidth: 5,
+    borderColor: '#fff',
+    padding: 50,
+    borderRadius: 20,
+  },
+  button: {
+    marginTop: 10
+  }
 });
